4ee4f7403aa0d933001d79649d83bdd63eafa227
Roxana Czimbal <roxana.czimbal@gmail.com>
2019-03-06 11:56:30 +0000

    modificat blink pt a rula pe motorase

--- blink.c
+++ blink.c
@@ -1,20 +1,38 @@
 #include <wiringPi.h>
 #include <stdio.h>
-#define LedPin 21
+#define IN1 21
+#define IN2 22
 
 int main(void){
 if(wiringPiSetup() == -1){
 	printf("setup wiringPi failed! \n");
 	return -1;
 }
-pinMode(LedPin, OUTPUT);
+pinMode(IN1, OUTPUT);
+pinMode(IN2, OUTPUT);
+int i=0;
 while(1){
-	digitalWrite(LedPin, LOW);
-	printf("on\n");
-	delay(1000);
-	digitalWrite(LedPin, HIGH);
-	printf("off\n");
-	delay(1000);
+i++;
+if(i>=100)
+	i=0;
+	digitalWrite(IN1, LOW);
+	digitalWrite(IN2, LOW);
+	printf("OFF\n");
+	delay(100-i);
+	digitalWrite(IN1, HIGH);
+	digitalWrite(IN2, LOW);
+	printf("SENS 1\n");
+	delay(i);
+/*
+	digitalWrite(IN1, LOW);
+	digitalWrite(IN2, LOW);
+	printf("OFF\n");
+	delay(5000);
+	digitalWrite(IN1, LOW);
+	digitalWrite(IN2, HIGH);
+	printf("SENS 2\n");
+	delay(5000);
+*/
 }
 return 0;
 }
